ffadaf35f0e1a65e35283bb527ab52676d600c8c
Ian Chai <ianchai@mmu.edu.my>
2021-02-19 14:38:44 +0800

    Added the variables for UI elements

--- app/src/main/java/com/lomaikai/localdatabaseexample/MainActivity.java
+++ app/src/main/java/com/lomaikai/localdatabaseexample/MainActivity.java
@@ -3,12 +3,26 @@ package com.lomaikai.localdatabaseexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editName, editPhone, editDate;
+    Button btnInsert, btnUpdate, btnDelete, btnView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Linking UI elements
+        editName=findViewById(R.id.editName);
+        editDate=findViewById(R.id.editDate);
+        editPhone=findViewById(R.id.editPhone);
+        btnInsert=findViewById(R.id.btnInsert);
+        btnDelete=findViewById(R.id.btnDelete);
+        btnUpdate=findViewById(R.id.btnUpdate);
+
     }
 }
